af9b944948aa0e87399bb7c25b19147b626771cf
Sheeba Yegambaram <helenjo29@gmail.com>
2018-05-17 19:28:36 -0600

    Initial check-in of module TestAutomation

---
+++ TestAutomation/NUnitTestClass.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+namespace TestAutomation
+{
+    [TestFixture()]
+    public class NUnitTestClass
+    {
+        [Test()]
+        public void ShouldValidateCorrectNum()
+        {
+            var sut = new Validate();
+            var isValid = sut.ValidityOf("X1234567");
+
+            Assert.IsTrue(isValid);
+        }
+    }
+}
---
+++ TestAutomation/Validate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace TestAutomation
+{
+        public class Validate
+        {
+            public bool ValidityOf(string membershipNumber)
+            {
+                var isIncorrectLength = membershipNumber == "" || membershipNumber.Length != 8;
+
+                if (isIncorrectLength)
+                {
+                    return false;
+                }
+
+                if (StartsWithALetterFollowedBy7Numbers(membershipNumber))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            private static bool StartsWithALetterFollowedBy7Numbers(string membershipNumber)
+            {
+                int dummy;
+
+                return char.IsLetter(membershipNumber[0]) &&
+                    int.TryParse(membershipNumber.Substring(1), out dummy);
+            }
+        }
+    }
